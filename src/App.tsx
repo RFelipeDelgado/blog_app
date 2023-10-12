cdd60ddd958636640627ecdd7d90a0bad77a5333
RFelipeDelgado <raulfeliped@hotmail.com>
2023-10-12 15:15:50 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import * as React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Blog from './components/Blog/Blog';
-import About from './components/About/About';
-import Header from './components/Header/Header';
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Blog from "./components/Blog/Blog";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import styled from "styled-components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header/>
-        <main>
+      <ContenedorPrincipal>
+        <Header />
+        <Main>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/blog' element={<Blog/>} />
-            <Route path='/about' element={<About/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/about" element={<About />} />
           </Routes>
-        </main>
-      </div>
+        </Main>
+      </ContenedorPrincipal>
     </BrowserRouter>
-  )
-}
+  );
+};
+
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  width: 90%;
+  max-width: 700px;
+`;
+
+const Main = styled.main`
+  background: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 8px 8px 5px rgba(129, 129, 129, 0.1);
+`
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react';
 import {NavLink} from 'react-router-dom'
+import styled from 'styled-components';
 
 const Header = () => {
     return (
-        <header>
-          <h1>My madafakin blog</h1>
+        <ContenedorHeader>
+          <Titulo>My madafakin blog</Titulo>
           <nav>
             <NavLink to='/'>Inicio</NavLink>
             <NavLink to='/blog'>Blog</NavLink>
             <NavLink to='/about'>About</NavLink>  
           </nav>
-        </header>
+        </ContenedorHeader>
     )
-}
+};
+
+const ContenedorHeader = styled.header`
+  text-align: center;
+  margin-bottom: 40px;
+
+`;
+
+const Titulo = styled.h1`
+  margin-bottom: 10px;
+  font-size: 26px;
+  text-transform: uppercase;
+`
 
 export default Header
